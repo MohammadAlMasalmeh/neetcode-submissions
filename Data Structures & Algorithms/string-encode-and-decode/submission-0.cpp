3a565ec375b867019d84c50964ede5222725125e
MohammadAlMasalmeh <malmasalmeh.2019@gmail.com>
2026-07-29 19:04:09 -0500

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for(auto& str : strs){
+            encoded += to_string(str.size()) + "@" + str;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i = 0;
+        while(i < s.size()){
+            int j = i;
+            while(s[j] != '@'){
+                j++;
+            }
+            int len = stoi(s.substr(i, j - i));
+            i = j+1;
+            ans.push_back(s.substr(i, len));
+            i+= len;
+        }
+        return ans;
+    }
+};
